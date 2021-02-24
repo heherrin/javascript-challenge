884d4c77e6d7310832c9e591a059951573692b08
Heath Herrington <heathherrington@comcast.net>
2021-02-23 18:20:56 -0600

    loaded data & changed background to moon man

--- UFO-Level-1/static/js/app.js
+++ UFO-Level-1/static/js/app.js
@@ -1,4 +1,23 @@
 // from data.js
 var tableData = data;
+//select buttons & date input
+var button = d3.select("#filter-btn");
+var dateinput = d3.select("#datetime");
 
-// YOUR CODE HERE!
+//console.log data
+console.log(tableData);
+
+//select data
+var aliensightings = d3.select("#ufo-table").select("tbody");
+
+// populate table with sighting data
+data.forEach((encounter) => {
+    var row = aliensightings.append("tr");
+    Object.entries(encounter).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+
+  
+
